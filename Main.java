b55c9c8e86d0f087860d304fe9ba1020d991476b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2021-03-11 08:00:00 +0100

    Num Operatos

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ class Main {
     public static void main(String[] args){
        // TYPE PROMOTION
         int num1 = 7;
-        num1++; // increment
+        // num1++; //post increment
+        // ++num1; // pre increement
         System.out.println(num1);
     }
 }
