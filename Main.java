e62fe26fd35b70f4f39bd9a3087c14a01a02fac2
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-03-18 00:35:09 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args){
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 59848l;
+      float num = 5.8f;
+      double d = 5.8;
+      char k = 'k';
+
+
+      float num2 = 5f;
+      float result = num + num2;
+      char c = 'D';
+      char e = 'D';
+      char d ="f";
+      boolean isHuman = false;
+        System.out.println(8+7);
+        System.out.println(result);
+    }
+}
