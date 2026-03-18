f80057428d067df8bc0edd66565ad88928c5d699
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-03-18 01:41:57 +0100

    working with hex and binaries

--- Main.java
+++ Main.java
@@ -1,21 +1,11 @@
 class Main {
     public static void main(String[] args){
-        int num1 = 9;
-        byte by = 127;
-        short sh = 558;
-        long l = 59848l;
-      float num = 5.8f;
-      double d = 5.8;
-      char k = 'k';
-
-
-      float num2 = 5f;
-      float result = num + num2;
-      char c = 'D';
-      char e = 'D';
-      char dd ='f';
-      boolean isHuman = false;
-        System.out.println(8+7);
-        System.out.println(result);
+       // literals
+       int num1 = 0b101;
+       int num2 = 0xEE;
+       long num3 = 100_00000_0000l;
+       System.out.println(num1);
+       System.out.println(num2);
+       System.out.println(num3);
     }
 }
