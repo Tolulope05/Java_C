8770c7804e62df857992fe6ed88ff1bab04d0a2b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-03-18 02:33:58 +0100

    type promotion

--- Main.java
+++ Main.java
@@ -1,23 +1,9 @@
 class Main {
     public static void main(String[] args){
-    //    // literals
-    //    int num1 = 0b101;
-    //    int num2 = 0xEE;
-    //    long num3 = 100_00000_0000l;
-    //    System.out.println(num1);
-    //    System.out.println(num2);
-    //    System.out.println(num3);
-       // TYPE CONVERSIONS
-       // int, float, double, long, char, boolean, string
-       byte b = 127;
-       int a = 257;
-        //   b = a // x b =(byte)a;
-        b =(byte)a;
-       a = b; // works
-float f = 5.6f;
-       int x = (int)f;
-       System.out.println(a);
-       System.out.println(x);
-
+       // TYPE PROMOTION
+       byte a = 10;
+       byte b = 30;
+      int result = a * b;
+       System.out.println(result);
     }
 }
