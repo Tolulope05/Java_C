be3291c9335c2993c1346d855675da540a1564b0
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-01 00:58:58 +0100

    feat: Type promotion

---
+++ Anu.java
@@ -0,0 +1,5 @@
+class Anu{
+    public static void main(String[] args){
+        System.out.println(1+1);
+    }
+}
--- Main.java
+++ Main.java
@@ -2,13 +2,9 @@ class Main {
     public static void main(String[] args){
        // TYPE PROMOTION
         int num1 = 7;
-        // num1++; //post increment
-        // ++num1; // pre increement
-        // int result = ++ num1;
-        int result2 = ++ num1;
-        int result3 = num1;
-        // System.out.println(result);
-        System.out.println(result2);
-        System.out.println(result3);
-    }
+        int num2 = 8;
+        boolean result = num1> num2;
+        System.out.println(result);
+        
+    } 
 }
