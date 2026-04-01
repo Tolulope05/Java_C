71857ac12b11c4bcb052925ed65a484930905b10
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-01 10:02:28 +0100

    feat: TENARY OPERATOR

--- Main.java
+++ Main.java
@@ -1,19 +1,17 @@
 class Main {
     public static void main(String[] args){
-       // TYPE PROMOTION
-        int x = 5;
-        int y  = 7;
-        int z = 8;
-        
-        // PRINT GREATEST NUMBER
-        if (x>y && x>z)
-            System.out.println("X");
-        else if (y>x && y>z) 
-            System.out.println("Y");
-        else if (z>x && z>y) 
-            System.out.println("Z");
-        else 
-            System.out.println("None of the conditionds checks out.");
+       // TERNARY OPERATOR
+        int n = 5;
+        int result = 0;
+        // if(n%2==0)
+        //     result = 10;
+        // else   
+        //     result = 20;
+
+        // TENEARY ? :
+            result = (n%2==0) ? 10 : 20;
+        System.out.println(result);
+
         
     } 
 }
