ea4bd58af28963d12ee542ec0a3370b045bcadac
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-06 08:48:44 +0100

    feat: OBJECTS in Flutter

--- Main.java
+++ Main.java
@@ -1,21 +1,19 @@
- class Calculator {
-int a; // what object knows  (property)
-    public int add(int ni, int n2){ // Add: what object does (function)
-        System.out.println("in add");
-        return ni + n2;
+class Computer {
+  public void  playMusic(){
+        // behanviour
+        System.out.println("Music Playing..");
+    }
+    public  String getMeAPen(int cost)
+    {
+        return "Pen";
     }
 }
-
 public class Main {
-    public static void main(String[] args) {
-
-        int num1 = 4;
-        int num2 = 3;
-
-        Calculator calc = new Calculator(); // How you create an object instance using new keyword.
-
-        int result = calc.add(num1, num2);
-        System.out.println(result);
+    public static void main(String[] args) { // start of execution
+Computer obj = new Computer();
+obj.playMusic();
+String str = obj.getMeAPen(10);
+System.out.println(str);
 
     }
 }
