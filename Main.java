035f61cfada8b0760f8ff36f731139111ab44e34
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-07 02:47:27 +0100

    Array of OBJECTs

--- Main.java
+++ Main.java
@@ -1,34 +1,37 @@
+ class Student {
+
+    int rollno;
+    String name;
+    int marks;
+
+}
+
 public class Main {
     public static void main(String[] args) {
-        // Define the dimensions of the 3D array
-        int rows = 3;
-        int cols = 4;
-        int depth = 2;
+        // Array of OBJECTs
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Navin";
+        s1.marks = 88;
 
-        // Initialize the 3D array with a specific value
-        int[][][] array3d = new int[depth][rows][cols];
-        for (int i = 0; i < depth; i++) {
-            for (int j = 0; j < rows; j++) {
-                for (int k = 0; k < cols; k++) {
-                    array3d[i][j][k] = i * rows * cols + j * cols + k + 1;
-                }
-            }
-        }
+        Student s2 = new Student();
+        s2.rollno = 2;
+        s2.name = "David";
+        s2.marks = 45;
 
-        // Print the 3D array
-        for (int i = 0; i < depth; i++) {
-            System.out.print("[");
-            for (int j = 0; j < rows; j++) {
-                System.out.print("[");
-                for (int k = 0; k < cols; k++) {
-                    System.out.print(array3d[i][j][k]);
-                    if (k < cols - 1) {
-                        System.out.print(" ");
-                    }
-                }
-                System.out.print("]");
-            }
-            System.out.println("]");
+        Student s3 = new Student();
+        s3.rollno = 3;
+        s3.name = "Mark";
+        s3.marks = 12;
+
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (int i = 0; i < students.length; i++) {
+            System.out.println( students[i].name + " : " + students[i].marks);
         }
     }
 }
