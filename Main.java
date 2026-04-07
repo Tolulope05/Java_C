121e7f67ac3c795dbe30f879002fe5237aed5624
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-07 01:20:33 +0100

    feat: Method overloading in JAVA

--- Main.java
+++ Main.java
@@ -1,25 +1,21 @@
 class Computer {
-  public void  playMusic(){
-        // behanviour
-        System.out.println("Music Playing..");
+    // METHOD OVERLOADING: Two or three nethids with same name but different paramters
+    public int add(int n1, int n2, int n3){
+        int result = n1+n2+n3;
+        return result;
     }
-    public  String getMeAPen(int cost)
-    {
-        if (cost >= 10) {
-            return "Pen";
-        } else {
-
-            return "Nothing";
-        }
+    public double add(double n1, int n2){
+        double result = n1+n2;
+        return result;
     }
 }
 public class Main {
     public static void main(String[] args) { // start of execution
-Computer obj = new Computer();
-obj.playMusic();
-String str = obj.getMeAPen(2);
-System.out.println(str);
-
+        Computer obj = new Computer();
+        int r1=    obj.add(5, 4,5);
+        double r2=    obj.add(5, 4);
+        System.out.println(r1);
+        System.out.println(r2);
     }
 }
 
