947c98ebf8b6c737d5a9bf9251f85b28510efc3a
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-07 02:19:34 +0100

    JAGGED ARRAY

--- Main.java
+++ Main.java
@@ -1,28 +1,26 @@
 
 public class Main {
     public static void main(String[] args) { // start of execution
-        //MULTI DIMENSIONAL ARRAYS
-        int nums[][]= new int[3][4]; // 3 row, 4 columns
-    //   int random = (int)Math.random(); // double value converted into int
-        for(int i =0; i<=2;i++){
-            for(int j=0;j<=3;j++){
+        //MULTI DIMENSIONAL ARRAYS (JAGGED ARRAY)
+             int nums[][]= new int[3][]; // 3 row, JAGGED ARRAY
+             // WE HAVE TO SPECIFY THE COLUMNS SIZES
+             nums[0] = new int[3];
+             nums[1] = new int[4];
+             nums[2] = new int[2];
+   
+
+        for(int i =0; i<nums.length;i++){
+            for(int j=0;j<nums[i].length;j++){
                 nums[i][j] = (int)(Math.random() * 10);
             }
         }
-        for(int i =0; i<=2;i++){
-            System.out.println("ROW: " + nums[i]);
-            for(int j=0;j<=3;j++){
-
-                System.out.print("    COLUMN: "+ nums[i][j]);
-            }
-            System.out.println();
-        }
+    
         for(int n[]: nums){
             for(int m : n){
                 System.out.print("PICKING VALUE "+ m + " ");
             }
             System.out.println();
-        }
+        } // EHANCED FORLOOP
     }
 }
 
