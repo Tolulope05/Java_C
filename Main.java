866ff9e7555ffc4fdbec50e33cc496d49bef9cfe
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-07 01:08:43 +0100

    feat: methods in a class

--- Main.java
+++ Main.java
@@ -5,14 +5,19 @@ class Computer {
     }
     public  String getMeAPen(int cost)
     {
-        return "Pen";
+        if (cost >= 10) {
+            return "Pen";
+        } else {
+
+            return "Nothing";
+        }
     }
 }
 public class Main {
     public static void main(String[] args) { // start of execution
 Computer obj = new Computer();
 obj.playMusic();
-String str = obj.getMeAPen(10);
+String str = obj.getMeAPen(2);
 System.out.println(str);
 
     }
