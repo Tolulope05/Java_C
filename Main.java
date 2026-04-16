cc9cc942f77fe7d9827bd80c1e707c333accf15e
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-16 08:52:48 +0100

    String buffer

--- Main.java
+++ Main.java
@@ -1,2 +1,11 @@
+public class Main {
 
-
+    public static void main(String[] args) {
+        // STRING BUFFER
+     StringBuffer sb = new StringBuffer("NAVIN");
+     // STRING BUFFER GIVES YOU A BUFFER SIZE 16 with () and 21 with ("NAVIN")
+    //  It gives extra 16 buffer to prevent relocation
+     System.out.println(sb.capacity());
+     
+    }
+}
