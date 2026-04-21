14f856fa4a1ddbb700c45ab02559d9525e0530cf
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 17:26:52 +0100

    inheritance starts

--- Main.java
+++ Main.java
@@ -1,20 +1,10 @@
 
 /**
- * Anonymous Object
+ * INHERITANCE
+ * - is, has
  */
-
-class A {
-  public A() {
-    System.out.println("OBJECT CREATED");
-  }
-
-  public void show() {
-    System.out.println("In a show");
-  }
-}
 
 public class Main {
   public static void main(String[] args) {
-    new A(); // anonymous object;
   }
 }
