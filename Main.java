08bc65ee283e0e94d2c85c6e53872d218f0bf5e9
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 14:34:20 +0100

    Encapsulation done

--- Main.java
+++ Main.java
@@ -1,19 +1,37 @@
- class Human {
-   private int age = 11;
-  private  String name = "Navin";
-  public int  getAge(){
-      return age;
-    }
- 
+class Human {
+  private int age = 11;
+  private String name = "Navin";
+
+  // FETCH VARIABLE
+  public int getAge() {
+    return age;
+  }
+
+  // FETCH VARIABLE
+  public String getName() {
+    return name;
+  }
+
+  // SET VALUE
+  public void setAge(int a) {
+    age = a;
+  }
+
+  // SET VALUE
+  public void setName(String n) {
+    name = n;
+
+  }
 
 }
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     Human obj = new Human();
-    // obj.age = 11; 
-    // obj.name = "Navin"; 
+    // obj.age = 11;
+    // obj.name = "Navin";
+    obj.setAge(30);
 
-    System.out.println(obj.getAge());
-    }
+    System.out.println(obj.getName() + " " + obj.getAge());
+  }
 }
