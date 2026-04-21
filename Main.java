236524f8f994b343a2fba9e2eea5bce10ba038e4
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 23:29:23 +0100

    INHERITANCE

--- Main.java
+++ Main.java
@@ -2,7 +2,30 @@
 /**
  * INHERITANCE
  * - is, has
+ * Parent - child, Super - sub, base - derived.
  */
+
+class Calc {
+  public int add(int n1, int n2) {
+
+    return n1 + n2;
+  }
+
+  public int sub(int n1, int n2) {
+
+    return n1 - n2;
+  }
+
+  public int mult(int n1, int n2) {
+
+    return n1 * n2;
+  }
+
+  public int div(int n1, int n2) {
+
+    return n1 / n2;
+  }
+}
 
 public class Main {
   public static void main(String[] args) {
