fcbdc3da9b8b9e4e9b088335e377929d8a922414
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 16:19:54 +0100

    STATIC VARIABLE

--- Main.java
+++ Main.java
@@ -1,43 +1,30 @@
 
 /**
- * CONSTRUCTOR OVERLOADING
- * - IT is called everytime you create an object, it will call the constructor
- * We can use defaul constructor and paratemrised constructor
+ * STATIC VARIABLE
+ * - Static variable is shared by all objects
  * 
- * -
  */
 
-class Human {
-  private int age; // INSTANCE VARIABLE
-  private String name; // INSTANCE VARIABLE
+class Mobile {
+  private String brand;
+  private int price;
+  private String name;
+  static String developer;
 
-  // public Human() { // deault
-  // System.out.println("IN CONSTRUCTOR");
-  // age = 12;
-  // name = "JOHN";
-  // }
-
-  // public Human(int a, String n) { // Parameterized Constructors
-  // age = a;
-  // name = n;
-
-  // }
-
-  public Human() {
+  public String getBrand() {
+    return brand;
   }
 
-  public Human(int age, String name) {
-    this.age = age;
-    this.name = name;
+  public void setBrand(String brand) {
+    this.brand = brand;
   }
 
-  public int getAge() {
-    return age;
+  public int getPrice() {
+    return price;
   }
 
-  public void setAge(int age) {
-    // THIS IS A KEYWORD THAT REPRESENT THE CURENT OBJECT
-    this.age = age;
+  public void setPrice(int price) {
+    this.price = price;
   }
 
   public String getName() {
@@ -45,22 +32,46 @@ class Human {
   }
 
   public void setName(String name) {
-    // THIS IS A KEYWORD THAT REPRESENT THE CURENT OBJECT
     this.name = name;
+  }
+
+  public Mobile(String brand, int price, String name) {
+    this.brand = brand;
+    this.price = price;
+    this.name = name;
+  }
+
+  public Mobile() {
+
+  }
+
+  public void show() {
+
+    System.out.println(brand + " : " + price + " : " + name + " : " + developer);
   }
 
 }
 
 public class Main {
   public static void main(String[] args) {
-    Human obj = new Human();
-    Human obj1 = new Human(18, "Tee");
-    // obj.age = 11;
-    // obj.name = "Navin";
-    // obj.setName("Tee");
-    // obj.setAge(30);
 
-    System.out.println("OBJ: " + obj.getName() + " " + obj.getAge());
-    System.out.println("OBJ1: " + obj1.getName() + " " + obj1.getAge());
+    Mobile mob1 = new Mobile();
+    Mobile mob2 = new Mobile();
+
+    Mobile.developer = "Tolylope"; // WILL UPDATE FOR ALL
+
+    mob1.setBrand("Apple");
+    mob1.setPrice(1500);
+    mob1.setName("SmartPhone");
+
+    mob2.setBrand("Samsung");
+    mob2.setPrice(1700);
+    mob2.setName("SmartPhone");
+
+    mob1.show();
+    mob2.show();
+
+    //
+
   }
 }
