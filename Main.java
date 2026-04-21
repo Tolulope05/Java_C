7b874ffb1eff6b5197445e08890b35870abf275c
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 14:49:39 +0100

    This Keyword

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 class Human {
-  private int age;
-  private String name;
+  private int age; // INSTANCE VARIABLE
+  private String name; // INSTANCE VARIABLE
 
   public int getAge() {
     return age;
   }
 
   public void setAge(int age) {
+    // THIS IS A KEYWORD THAT REPRESENT THE CURENT OBJECT
     this.age = age;
   }
 
@@ -15,6 +16,7 @@ class Human {
   }
 
   public void setName(String name) {
+    // THIS IS A KEYWORD THAT REPRESENT THE CURENT OBJECT
     this.name = name;
   }
 
@@ -25,6 +27,7 @@ public class Main {
     Human obj = new Human();
     // obj.age = 11;
     // obj.name = "Navin";
+    obj.setName("Tee");
     obj.setAge(30);
 
     System.out.println(obj.getName() + " " + obj.getAge());
