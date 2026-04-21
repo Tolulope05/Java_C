60df3dfec4189ed397ca4cdbaf977a9514e2de5b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 23:36:25 +0100

    MULTI INHERITANCE

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 
 /**
- * INHERITANCE
+ * MULTI LEVEL INHERITANCE
  * - is, has
  * Parent - child, Super - sub, base - derived.
  * Using extend keyword
+ * Calc -> Adv Calc -> VeryAdvCalc
  */
 
 public class Main {
@@ -13,8 +14,14 @@ public class Main {
     int r2 = obj.sub(10, 4);
     System.out.println(r1 + " : " + r2);
 
-    AdvCalc advCalc = new AdvCalc();
-    int r3 = advCalc.add(4, 6);
+    AdvCalc obj2 = new AdvCalc();
+    int r3 = obj2.add(4, 6);
     System.out.println("ADVANCE CALC " + r3);
+
+    VeryAdvCalc obj3 = new VeryAdvCalc();
+    int r5 = obj3.add(3, 5);
+    double r6 = obj3.power(5, 4);
+    System.out.println("VERY ADVANCE CALC " + r5 + " : " + r6);
+
   }
 }
---
+++ VeryAdvCalc.java
@@ -0,0 +1,6 @@
+public class VeryAdvCalc extends AdvCalc {
+    public double power(int n1, int power) {
+
+        return Math.pow(n1, power);
+    }
+}
