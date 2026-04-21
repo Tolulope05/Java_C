a67d7aab8766f371ea5b2747b8d9090db4926ce3
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 14:58:49 +0100

    Constructors

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
+
+/**
+ * CONSTRUCTORS HELP SET DEFAULT VALUES
+ * - IT is called everytime you create an object, it will call the constructor
+ * -
+ */
+
 class Human {
   private int age; // INSTANCE VARIABLE
   private String name; // INSTANCE VARIABLE
+
+  public Human() {
+    System.out.println("IN CONSTRUCTOR");
+    age = 12;
+    name = "JOHN";
+  }
 
   public int getAge() {
     return age;
@@ -25,10 +38,11 @@ class Human {
 public class Main {
   public static void main(String[] args) {
     Human obj = new Human();
+    Human obj1 = new Human();
     // obj.age = 11;
     // obj.name = "Navin";
-    obj.setName("Tee");
-    obj.setAge(30);
+    // obj.setName("Tee");
+    // obj.setAge(30);
 
     System.out.println(obj.getName() + " " + obj.getAge());
   }
