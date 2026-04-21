2e32cc4f33a1cae6aeb8766c40e04b6a776ef2ca
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-04-21 14:19:24 +0100

    feat: Encapsulation part 1

--- Main.java
+++ Main.java
@@ -1,3 +1,17 @@
+ class Human {
+   private int age;
+  private  String name;
+  // EVERYTIME YOU CREATE AN NSTANCE VIARIABLE< MAKE IT PRIVATE
+  // THEY SHOULDNT BE ABLE TO ACCESS IT DIRECTLY BUT THROUGH METHODS
+
+}
+
 public class Main {
     public static void main(String[] args) {
+    Human obj = new Human();
+    obj.age = 11; // WONT WORK 
+    obj.name = "Navin"; // WONT WORK
+
+    System.out.println(obj.name); // WONT WORK
+    }
 }
