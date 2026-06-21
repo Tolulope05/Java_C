1656fda066fc5b6e36852e7f53289991a5457679
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-06-22 00:12:13 +0100

    THIS AND SUPER METHOD

--- Main.java
+++ Main.java
@@ -1,43 +1,38 @@
-
 /**
- * MULTIPLE INHERITANCE
- * 
+ * THIS AND SUPER METHOD
+ * - in every constructor, the default statement is super
+ * -
  */
 
 class A {
-  // method logData 
-  public void logData(){
-    System.out.println("Log Class A");
+  public A() {
+    super();
+    System.out.println("in A");
+  }
+
+  public A(int n) {
+    super();
+    System.out.println("in A int");
   }
 }
 
-class B {
-  // method logData 
-  public void logData(){
-    System.out.println("Log Class B");
+class B extends A {
+  public B() {
+    super();
+
+    System.out.println("in B");
+  }
+
+  public B(int n) {
+    super();
+
+    System.out.println("in B int");
   }
 }
-class C  extends A,B{
-  // when it extends both, We try to access method logData.
-  // It doesnt know which to call
-} // Ambiguity Problem
 
 public class Main {
   public static void main(String[] args) {
-    Calc obj = new Calc();
-    int r1 = obj.add(3, 2);
-    int r2 = obj.sub(10, 4);
-    System.out.println(r1 + " : " + r2);
-
-    AdvCalc obj2 = new AdvCalc();
-    int r3 = obj2.add(4, 6);
-    System.out.println("ADVANCE CALC " + r3);
-
-    VeryAdvCalc obj3 = new VeryAdvCalc();
-    int r5 = obj3.add(3, 5);
-    double r6 = obj3.power(5, 4);
-    double r7 = obj3.div(6,2);
-    System.out.println("VERY ADVANCE CALC " + r5 + " : " + r6 + " : " + r7);
+    B obj = new B();
 
   }
 }
