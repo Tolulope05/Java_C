85a638fdde9f49a5b5e99134ebbfe2650516fc9b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-06-22 02:40:36 +0100

    Object class equals toString hashcode

--- Main.java
+++ Main.java
@@ -1,13 +1,63 @@
+import other.A;
 
-// final keyword - variable, method, class
-// - The variable once assigned cannot be changed. To make it constant
-// - Once you make a class final, you are stopping the inheritance, nobody can extends it.
-// - When you make your method final, no one can overide it
+class Laptop {
+  int price;
+  String model;
+
+  @Override
+  public String toString() {
+    return "Laptop [price=" + price + ", model=" + model + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + price;
+    result = prime * result + ((model == null) ? 0 : model.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Laptop other = (Laptop) obj;
+    if (price != other.price)
+      return false;
+    if (model == null) {
+      if (other.model != null)
+        return false;
+    } else if (!model.equals(other.model))
+      return false;
+    return true;
+  }
+
+  // public boolean equals(Laptop other) {
+  // // if (this.model.equals(other.model) && this.price == other.price)
+  // // return true;
+  // // else
+  // // return false;
+  // return this.model.equals(other.model) && this.price == other.price;
+
+  // }
+
+}
 
 public class Main {
   public static void main(String[] args) {
-    final int num = 8;
-    num = 9;
-    System.out.println(num);
+    Laptop obj = new Laptop();
+    obj.model = "Lenovo yoga";
+    obj.price = 1000;
+    Laptop obj2 = new Laptop();
+    obj2.model = "Lenovo yoga";
+    obj2.price = 1000;
+
+    boolean result = obj.equals(obj2);
+    System.out.println(result);
   }
 }
