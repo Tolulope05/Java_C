e66d67d7f1efb0114d820195acd759eaf33da26c
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-06-22 02:57:29 +0100

    Wrapper Class

--- Main.java
+++ Main.java
@@ -1,22 +1,28 @@
-class A {
-  public void show1() {
-    System.out.println("in A show");
-  }
-}
-
-class B extends A {
-  public void show2() {
-    System.out.println("in B show");
-  }
-}
 
 public class Main {
   public static void main(String[] args) {
-    A obj = new B(); // Upcasting
-    obj.show1();
+    int num = 7; // Primitive
+    Integer num1 = num; // boxing - Taking a primitive value and storing it in a wrapper object.
+    System.out.println(num1);
+    char char1 = 'f';
+    Character char2 = char1; // boxing
+    System.out.println(char2);
+    double dob1 = 6.6;
+    Double dob2 = dob1; // boxing
+    System.out.println(dob2);
 
-    B obj1 = (B) obj; // Downcasting
-    obj1.show2();
+    // UNBOXING
+    int num2 = num1.intValue(); // unboxing
+    char char3 = char2.charValue(); // unboxing
+    double dob3 = dob2.doubleValue(); // unboxing \
+
+    // auto boxing
+    // if its stored automatically
+    int num4 = num1; // autounboxing
+
+    String str = "12";
+    int num3 = Integer.parseInt(str);
+    System.out.println(num3);
 
   }
 }
