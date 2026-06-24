91dc6d6108d55f8a12d8171e14448ee188ffcfb5
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-06-24 01:05:49 +0100

    Reversing a string

--- Main.java
+++ Main.java
@@ -1,28 +1,30 @@
+/**
+ * REVERSING A STRING
+ */
 
 public class Main {
   public static void main(String[] args) {
-    int num = 7; // Primitive
-    Integer num1 = num; // boxing - Taking a primitive value and storing it in a wrapper object.
-    System.out.println(num1);
-    char char1 = 'f';
-    Character char2 = char1; // boxing
-    System.out.println(char2);
-    double dob1 = 6.6;
-    Double dob2 = dob1; // boxing
-    System.out.println(dob2);
+    String var1 = "Tolulope";
+    String reversed = reverseString(var1);
 
-    // UNBOXING
-    int num2 = num1.intValue(); // unboxing
-    char char3 = char2.charValue(); // unboxing
-    double dob3 = dob2.doubleValue(); // unboxing \
+    System.out.println(reversed);
+  }
 
-    // auto boxing
-    // if its stored automatically
-    int num4 = num1; // autounboxing
+  public static String reverseString(String text) {
+    String reversed = "";
 
-    String str = "12";
-    int num3 = Integer.parseInt(str);
-    System.out.println(num3);
+    for (int i = text.length() - 1; i >= 0; i--) {
+      reversed = reversed + text.charAt(i);
+    }
 
+    return reversed;
+  }
+
+  public static String reverseStringTwo(String input) {
+    StringBuilder result = new StringBuilder();
+    for (int i = input.length() - 1; i >= 0; i--) {
+      result.append(input.charAt(i));
+    }
+    return result.toString();
   }
 }
