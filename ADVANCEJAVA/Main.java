08c60847a90ae6d126fd5872550404bf1261f473
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-02 21:39:54 +0100

    Interface

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,35 +1,39 @@
 /**
- * ABSTRACT CLASS AND ANONYMOUS INNER CLASS
+ * INTERFACE
+ * - Just show you the design
+ * - We use the keyword implement
  */
 public class Main {
     public static void main(String[] args) {
-        A obj = new A() {
-            public void show() {
-                System.out.println("In NEW SHOW");
-            } // ANONYMOUS INNER CLASS
 
-            public void config() {
-                System.out.println("In NEW CONFIG");
-            } // ANONYMOUS INNER CLASS
-        };
+        A obj;
+        obj = new B();
         obj.show();
+        obj.config();
+        System.out.println(A.age);
+        System.out.println(A.area);
+
     }
 }
 
-abstract class A {
-    public abstract void show();
-
-    public abstract void config();
-}
-
-class B extends A {
+class B implements A {
 
     public void show() {
-        System.out.println("IN B SHOW");
-    } //
+        System.out.println("IN SHOW");
+    }
 
     public void config() {
-        System.out.println("In B NEW CONFIG");
-    } //
+        System.out.println("IN CONFIG");
 
+    }
 }
+
+interface A {
+    int age = 0; // by default thery are final and static
+    String area = "Lagos"; // by default thery are final and static
+
+    void show();
+
+    void config(); // BENDHIND THE SCENE public abstract is there.
+}
+// WE GET INTERFACE SO THAT WE CAN DESIGN SOMETHING
