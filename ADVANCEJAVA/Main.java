2663cf43f162bdeabe91e709964042081c0ac922
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-02 10:12:59 +0100

    ABSTRACT CLASS AND ANONYMOUS INNER CLASS

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,25 +1,35 @@
 /**
- * ANONYMOUS INNER CLASS
+ * ABSTRACT CLASS AND ANONYMOUS INNER CLASS
  */
 public class Main {
     public static void main(String[] args) {
         A obj = new A() {
             public void show() {
                 System.out.println("In NEW SHOW");
-            }
+            } // ANONYMOUS INNER CLASS
+
+            public void config() {
+                System.out.println("In NEW CONFIG");
+            } // ANONYMOUS INNER CLASS
         };
         obj.show();
     }
 }
 
-class A {
-    public void show() {
-        System.out.println("IN A SHOW");
-    }
+abstract class A {
+    public abstract void show();
+
+    public abstract void config();
 }
 
-// class B extends A {
-// public void show() {
-// System.out.println("IN B SHOW");
-// }
-// }
+class B extends A {
+
+    public void show() {
+        System.out.println("IN B SHOW");
+    } //
+
+    public void config() {
+        System.out.println("In B NEW CONFIG");
+    } //
+
+}
