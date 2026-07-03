1bc3089bd9bfe6655a1b2ab6688529f00873c192
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-03 20:37:33 +0100

    LAMBDA EXPRESSION

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,16 +1,19 @@
 /**
- * FUNCTIONAL INTERFACE
+ * LAMBDA EXPRESSION
  * 
  */
 public class Main {
     public static void main(String[] args) {
-        //
-        A obj = new A() {
-            public void show() {
-                System.out.println("This is an anonymous class");
-            }
-        };
+
+        // A obj = new A() {
+        // public void show() {
+        // System.out.println("This is an anonymous class");
+        // }
+        // };
+        A obj = () -> System.out.println("This is an anonymous class");
+
         obj.show();
+
     }
 }
 
