6887a4169fa698d876e047c6695c0cbc7b633061
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-03 07:46:56 +0100

    Annotation @override @Deprecated etc

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,46 +1,22 @@
 /**
- * ENUM CLASS WITH CUSTOMISED CONSTRUCTORS
- * - ordinal is used to get the location of the constsnt in the enum.
- * - values() is used to know all the list in the enum.
+ * ANOTATION
  */
 public class Main {
     public static void main(String[] args) {
-
-        // Laptop lap = Laptop.Macbook;
-        // System.out.println(s.getClass().getSuperclass()); //class java.lang.Enum
-        // System.out.println(lap + " : " + lap.getPrice());
-
-        for (int index = 0; index < Laptop.values().length; index++) {
-            Laptop laptop = Laptop.values()[index];
-            System.out.println(laptop + " : " + laptop.getPrice());
-
-        }
-
+        B obj = new B();
+        obj.showTheMethodWhichBelongToThisClass();
     }
 }
 
-enum Laptop {
-    Macbook(2000), XPS(500), Surface, ThinkPad(1800); // NAMED CONSTANTS
-
-    private Laptop(int price) {
-        this.price = price;
-        System.out.println("In priced laptop: " + this.name());
-
+class A {
+    public void showTheMethodWhichBelongToThisClass() {
+        System.out.println("IN A SHOW");
     }
+}
 
-    private Laptop() {
-        price = 500;
-        System.out.println("In laptop: " + this.name());
+class B extends A {
+    @Override // ANNOTATION: I am trying to override the method
+    public void showTheMethodWhichBelongToThisClass() {
+        System.out.println("IN B SHOW");
     }
-
-    private int price;
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
 }
