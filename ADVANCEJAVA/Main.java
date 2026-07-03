771eeed9286df53b61dc23182a3fe7a7b86791e2
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-03 01:29:49 +0100

    Switch and if elase with enums

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,17 +1,41 @@
 /**
- * ENUMS
+ * ENUMS (Switch and If else)
  * - ordinal is used to get the location of the constsnt in the enum.
  * - values() is used to know all the list in the enum.
  */
 public class Main {
     public static void main(String[] args) {
-        int i = 5;
+
         Status s = Status.Pending;
-        System.out.println(s);
-        System.out.println(s.ordinal()); // 2
-        Status[] statuses = s.values();
-        for (Status ss : statuses) {
-            System.out.println(s.ordinal() + ". " + ss);
+        if (s == Status.Running) {
+            System.out.println("All Good");
+        } else if (s == Status.Failed) {
+
+            System.out.println("Try again");
+
+        } else if (s == Status.Pending) {
+
+            System.out.println("Please wait");
+        } else {
+            System.out.println("Done");
+        }
+
+        switch (s) {
+            case Success:
+                System.out.println("Switch Success");
+                break;
+            case Pending:
+                System.out.println("Switch Pending");
+                break;
+            case Failed:
+                System.out.println("Switch Failed");
+                break;
+            case Running:
+                System.out.println("Switch Running");
+                break;
+
+            default:
+                break;
         }
     }
 }
