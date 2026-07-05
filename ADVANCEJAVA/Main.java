ffec5da09edb67326c7e2c137db910ca96d5354f
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-05 17:11:18 +0100

    TRY WITH RESOURCES

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,40 +1,21 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
 /**
- * BUFFEREDREADER AND SCANNER
+ * TRY WITH RESOURCES
  * -
  */
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Enter a number:");
-        try {
-            // // THIS SHOWS ASCII VALUES
-            // int num = System.in.read(); // Gives ASCII value for the number you enter
-            // System.out.println(num);
-
-            // // BETTER WAY (BUFFEREDREADER)
-            // InputStreamReader in = new InputStreamReader(System.in);
-            // BufferedReader bf = new BufferedReader(in);
-            // int num = Integer.parseInt(bf.readLine());
-            // System.out.println(num);
-            // bf.close(); // ALWAYS CLOSE RESOURCES
-
-            // // READING A FILE
-            // FileReader f = new FileReader("hello.txt");
-            // BufferedReader bf = new BufferedReader(f);
-            // System.out.println(bf.readLine());
-
-            // // ANOTHER WAY (SCANNER)
-            // Scanner sc = new Scanner(System.in);
-            // int val = sc.nextInt();
-            // System.out.println(val);
-            // sc.close();
-
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(System.in))) {
+            System.out.println(bf.readLine());
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
         }
+        // finally {
+        // // WHEN YOU WANT TO CLOSE A RESOURCE, USE FINALLY
+        // // bf.close();
+        // }
     }
 }
