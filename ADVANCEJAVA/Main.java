09061f868ba0d0ae7d1de01a91474cb4deb78e7b
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-05 15:36:11 +0100

    BUFFEREDREADER AND SCANNER

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -10,20 +11,30 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Enter a number:");
         try {
+            // // THIS SHOWS ASCII VALUES
             // int num = System.in.read(); // Gives ASCII value for the number you enter
             // System.out.println(num);
-            // BETTER WAY (BUFFEREDREADER)
-            InputStreamReader in = new InputStreamReader(System.in);
-            BufferedReader bf = new BufferedReader(in);
-            int num = Integer.parseInt(bf.readLine());
-            System.out.println(num);
-            bf.close(); // ALWAYS CLOSE RESOURCES
-            // ANOTHER WAY (SCANNER)
-            Scanner sc = new Scanner(System.in);
+
+            // // BETTER WAY (BUFFEREDREADER)
+            // InputStreamReader in = new InputStreamReader(System.in);
+            // BufferedReader bf = new BufferedReader(in);
+            // int num = Integer.parseInt(bf.readLine());
+            // System.out.println(num);
+            // bf.close(); // ALWAYS CLOSE RESOURCES
+
+            // // READING A FILE
+            // FileReader f = new FileReader("hello.txt");
+            // BufferedReader bf = new BufferedReader(f);
+            // System.out.println(bf.readLine());
+
+            // // ANOTHER WAY (SCANNER)
+            // Scanner sc = new Scanner(System.in);
+            // int val = sc.nextInt();
+            // System.out.println(val);
+            // sc.close();
 
         } catch (Exception e) {
             System.out.println(e.getStackTrace());
-            // TODO: handle exception
         }
     }
 }
