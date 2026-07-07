92c636c91c18bf0055d30077612e3174569d7c10
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-07 19:08:37 +0100

    CLEAN

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,43 +1,8 @@
 
 /**
- * RACE CONDITIONS IN threads
- * - safeguard synchronized
+ * ARRAYLIST
  */
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Counter c = new Counter();
-        Runnable objA = () -> {
-            for (int i = 0; i < 1000; i++) {
-                c.increment();
-            }
-
-        };
-        // B objB = new B();
-        Runnable objB = () -> {
-            for (int i = 0; i < 1000; i++) {
-                c.increment();
-
-            }
-
-        };
-
-        Thread t1 = new Thread(objA);
-        Thread t2 = new Thread(objB);
-        t1.start(); // start a new thread
-        t2.start(); // start a new theead
-
-        t1.join();
-        t2.join();
-
-        System.out.println(c.count);
-
-    }
-}
-
-class Counter {
-    int count;
-
-    public synchronized void increment() {
-        count++;
+    public static void main(String[] args) {
     }
 }
