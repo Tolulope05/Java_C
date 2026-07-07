cdef5aa82cfe9ac57cd65bd9c6c60d77eb4d76ae
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-07 14:49:15 +0100

    thread Priority and sleep

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -3,12 +3,25 @@
  * THREAD
  * - start method to start the thread, We need to have trhe run method in the
  * class we want to thread
+ * objB.setPriority(Thread.MAX_PRIORITY);
+ * try {
+ * Thread.sleep(10);
+ * } catch (InterruptedException e) {
+ * e.printStackTrace();
+ * }
+ * 
  */
 public class Main {
     public static void main(String[] args) {
         A objA = new A();
         B objB = new B();
+        objB.setPriority(Thread.MAX_PRIORITY);
         objA.start(); // start a new thread
+        try {
+            Thread.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         objB.start(); // start a new theead
 
     }
@@ -18,6 +31,11 @@ class A extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println(i + ": HI");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -26,6 +44,11 @@ class B extends Thread {
     public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println(i + ":Hello");
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
