3bf4dc37f9914705d0c71ad4a6ff436bef78b8b1
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-07 15:42:35 +0100

    Thread race

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,73 +1,43 @@
 
 /**
- * THREAD & RUNNABLE (LAMLAD VERSION)
- * - start method to start the thread, We need to have trhe run method in the
- * class we want to thread
- * objB.setPriority(Thread.MAX_PRIORITY);
- * try {
- * Thread.sleep(10);
- * } catch (InterruptedException e) {
- * e.printStackTrace();
- * }
- * 
- * RUNNABLE is an interface that makes thread easy to implement
- * 
+ * RACE CONDITIONS IN threads
+ * - safeguard synchronized
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
         Runnable objA = () -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(i + ": HI");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            for (int i = 0; i < 1000; i++) {
+                c.increment();
             }
 
         };
         // B objB = new B();
         Runnable objB = () -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println(i + ": HI");
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            for (int i = 0; i < 1000; i++) {
+                c.increment();
+
             }
 
         };
+
         Thread t1 = new Thread(objA);
         Thread t2 = new Thread(objB);
         t1.start(); // start a new thread
         t2.start(); // start a new theead
 
+        t1.join();
+        t2.join();
+
+        System.out.println(c.count);
+
     }
 }
 
-// class A implements Runnable {
-// public void run() {
-// for (int i = 0; i < 5; i++) {
-// System.out.println(i + ": HI");
-// try {
-// Thread.sleep(10);
-// } catch (InterruptedException e) {
-// e.printStackTrace();
-// }
-// }
-// }
-// }
+class Counter {
+    int count;
 
-// class B implements Runnable {
-// public void run() {
-// for (int i = 0; i < 5; i++) {
-// System.out.println(i + ":Hello");
-// try {
-// Thread.sleep(10);
-// } catch (InterruptedException e) {
-// e.printStackTrace();
-// }
-// }
-// }
-// }
+    public synchronized void increment() {
+        count++;
+    }
+}
