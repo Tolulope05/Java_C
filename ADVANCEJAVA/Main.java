952d140273e5b15f18d3a19880aab3ba0517c74d
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-08 06:39:36 +0100

    ArrayList

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,8 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
- * ARRAYLIST
+ * Collection - List, Queue, Set
+ * - List : ArrayList, LinkedList
+ * - Queue : DeQueue
+ * - Set : HashSet, Linked HashSe
  */
 public class Main {
     public static void main(String[] args) {
+        // Collection<Integer> nums = new ArrayList<Integer>();
+        List<Integer> nums = new ArrayList<Integer>();
+        nums.add(6);
+        nums.add(5);
+        nums.add(8);
+        nums.add(2);
+        nums.add(6);
+        for (int n = 0; n < nums.size(); n++) {
+            // System.out.println(nums.toArray()[n]);
+            // System.out.println(nums.get(n)); // Same thing is its a list
+            System.out.println(nums.indexOf(6));
+        }
+
+        System.out.println(nums);
     }
 }
