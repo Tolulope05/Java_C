276e989165f10f4d2186153442441bbca0815c0f
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-08 23:37:36 +0100

    Comparable and comparator

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -9,22 +9,47 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-        Comparator comp = new Comparator<String>() {
-            public int compare(String i, String j) {
-                if (i.length() > j.length()) {
-                    return 1;
-                }
-                return -1;
-            }
-        };
-        List<String> values = new ArrayList<>();
-        values.add("Tolu");
-        values.add("Aliya");
-        values.add("Ope");
-        values.add("Fakunle");
+        // Comparator comp = new Comparator<Student>() {
+        // public int compare(Student i, Student j) {
+        // if (i.age > j.age) {
+        // return 1;
+        // }
+        // return -1;
+        // }
+        // };
+        Comparator<Student> comp = (i, j) -> i.age > j.age ? 1 : -1;
+        List<Student> studs = new ArrayList<>();
+        studs.add(new Student(21, "Navin"));
+        studs.add(new Student(12, "Aliya"));
+        studs.add(new Student(18, "Ope"));
+        studs.add(new Student(20, "Fakunle"));
 
-        Collections.sort(values, comp);
-        System.out.println(values);
+        Collections.sort(studs, comp); // Comparator
+        Collections.sort(studs); // Comparable
+        System.out.println(studs);
 
     }
+}
+
+class Student implements Comparable<Student> {
+    int age;
+    String name;
+
+    public Student(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+
+    public int compareTo(Student that) {
+        if (this.age > that.age) {
+            return 1;
+        }
+        return -1;
+    }
+
 }
