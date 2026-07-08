23c0fa5a9b68064d948ba23280386820377c5704
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-08 06:46:38 +0100

    HAshSet and TreeeSet

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,28 +1,42 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Collection - List, Queue, Set
  * - List : ArrayList, LinkedList
  * - Queue : DeQueue
  * - Set : HashSet, Linked HashSe
+ * - SET
+ * HashSet supports unique values
+ * HashSet doesnt have indexed value
+ * if you need a sorted Value, use TreeSet
+ * 
  */
 public class Main {
     public static void main(String[] args) {
         // Collection<Integer> nums = new ArrayList<Integer>();
-        List<Integer> nums = new ArrayList<Integer>();
+        // Set<Integer> nums = new HashSet<Integer>();
+        Set<Integer> nums = new TreeSet<Integer>(); // Sorted Value
         nums.add(6);
         nums.add(5);
         nums.add(8);
         nums.add(2);
-        nums.add(6);
         for (int n = 0; n < nums.size(); n++) {
             // System.out.println(nums.toArray()[n]);
             // System.out.println(nums.get(n)); // Same thing is its a list
-            System.out.println(nums.indexOf(6));
         }
 
         System.out.println(nums);
+
+        Iterator<Integer> values = nums.iterator();
+        // System.out.println(values.next());
+        while (values.hasNext()) {
+            System.out.println(values.next());
+        }
     }
 }
