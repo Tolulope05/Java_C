f78f006d238561beffe6ee0d0366759a6d37d22a
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-09 17:56:37 +0100

    PARALLEL STREAM

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,27 +1,44 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
  * STEAM API
+ * PARALLEL STREAM
+ * - when values are not independent and its a lot faster.
  * 
  */
 public class Main {
     public static void main(String[] args) {
-        List<Integer> nums = Arrays.asList(4, 5, 7, 3, 2, 6);
+        int size = 10_000;
+        // List<Integer> nums = Arrays.asList(4, 5, 7, 3, 2, 6);
+        List<Integer> nums = new ArrayList<>(size);
+        Random ran = new Random();
+        for (int index = 0; index < size; index++) {
+            nums.add(ran.nextInt(100));
 
-        // Stream<Integer> s1 = nums.stream();
-        // // s1.forEach((n) -> System.out.println(n));
-        // Stream<Integer> s2 = s1.filter(n -> n % 2 == 0);
-        // // s2.forEach((n) -> System.out.println(n));
-        // Stream<Integer> s3 = s2.map(w -> w * 2);
-        // int result = s3.reduce(0, (p, n) -> p + n);
-        // // s3.forEach((n) -> System.out.println(n));
-        // System.out.println(result);
+        }
+        long startSeq = System.currentTimeMillis();
+        // System.out.println(nums);
+        int sum1 = nums.stream().map(i -> 1 * 2).reduce(0, (p, b) -> p + b);
+        int sum2 = nums.stream().map(i -> i * 2)
+                .mapToInt(i -> i)
+                .sum();
+        long endSeq = System.currentTimeMillis();
+        long startPara = System.currentTimeMillis();
 
-        int result = nums.stream().filter(n -> n % 2 == 0).map(n -> n * 2).reduce(0, (p, n) -> p + n);
-        System.out.println(result);
+        int sum3 = nums.parallelStream().map(i -> i * 2)
+                .mapToInt(i -> i)
+                .sum();
+        long endPara = System.currentTimeMillis();
+
+        // System.out.println("Sum 1: " + sum1 + " Sum 2: " + sum2 + " Sum 3: " + sum3);
+        System.out.println(" Sum 2: " + sum2 + " Sum 3: " + sum3);
+        System.out.println("Seq : " + (endSeq - startSeq));
+        System.out.println("Para : " + (endPara - startPara));
 
     }
 }
