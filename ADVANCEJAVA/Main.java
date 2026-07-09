a138e6eaecf700b15fc9fbb30e2591679b64e874
Tolulope Fakunle <fakunletolulope05@gmail.com>
2026-07-10 00:37:25 +0100

    Constructor refernce

--- ADVANCEJAVA/Main.java
+++ ADVANCEJAVA/Main.java
@@ -1,26 +1,54 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
-import java.util.function.Consumer;
-import java.util.stream.Stream;
 
 /**
- * METHOD REFERNCE
+ * CONSTRUCTOR REFERNCE
  * - passing a method name inside the method to use it
  * 
  */
 public class Main {
     public static void main(String[] args) {
-
         List<String> names = Arrays.asList("Navin", "Hush", "John");
-        List<String> uNames = names.stream().map((e) -> e.toUpperCase()).toList();
-        List<String> lNames = uNames.stream().map(String::toLowerCase).toList(); // Method refernce
-        System.out.println(uNames);
-        System.out.println(lNames);
+        List<Student> students = new ArrayList<>();
+        // for (String name : names) {
+        // students.add(new Student(name));
+        // } // option 1
+        // students = names.stream().map(n -> new Student(n)).toList(); // option 2
+        students = names.stream().map(Student::new).toList(); // option 3 Constructor Refernce
+        students.forEach(System.out::println);
+    }
+}
 
-        lNames.forEach(System.out::println);
+class Student {
+    private int age;
+    private String name;
 
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
     }
 }
